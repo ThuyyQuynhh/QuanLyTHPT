7a0dd1c95f6c1e2998cb63e01f0d4525bd6daaa2
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-11 09:16:09 +0700

    DA_Load du lieu ra bang

---
+++ QuanLyTHPT/Diemcanhan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyTHPT.Data;
+namespace QuanLyTHPT
+{
+    public partial class Diemcanhan : DevExpress.XtraEditors.XtraUserControl
+    {
+        public Diemcanhan()
+        {
+            InitializeComponent();
+        }
+        DataProvider dataProvider = new DataProvider();
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HocSinh_load(object sender, EventArgs e)
+        {
+            string query = "select *from HocSinh";
+            dtgHS.DataSource = dataProvider.GetDataTable(query);
+        }
+        private void Diemcanhan_Load(object sender, EventArgs e)
+        {
+            string query = "select *from HocSinh";
+            dtgHS.DataSource = dataProvider.GetDataTable(query);
+        }
+    }
+}
