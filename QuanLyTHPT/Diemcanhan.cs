4eaa1435b77328f7c2846f44cb4e3791909a182e
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-11 23:18:22 +0700

    DA_Xu ly thao tac trong bang

--- QuanLyTHPT/Diemcanhan.cs
+++ QuanLyTHPT/Diemcanhan.cs
@@ -37,5 +37,23 @@ namespace QuanLyTHPT
             string query = "select *from HocSinh";
             dtgHS.DataSource = dataProvider.GetDataTable(query);
         }
+        private void dtgHS_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = dtgHS.CurrentRow.Index;
+            txHoten.Text = dtgHS.Rows[i].Cells[1].Value.ToString();
+            string queryTenLop = "select L.TenLop from HocSinh HS, Lop L where HS.MaLop = L.MaLop and L.MaLop = '" + dtgHS.Rows[i].Cells[5].Value.ToString() + "'";
+            DataTable dataTenLop = dataProvider.GetDataTable(queryTenLop);
+            txLop.Text = dataTenLop.Rows[0][0].ToString();
+            string query = "select M.TenMon, D.Diem from MonHoc M, Diem D where M.MaMon = D.MaMon and D.MaHS = '" + dtgHS.Rows[i].Cells[0].Value.ToString() + "'";
+            DataTable data = dataProvider.GetDataTable(query);
+            txToan.Text = data.Rows[0][1].ToString();
+            txTin.Text = data.Rows[1][1].ToString();
+            txly.Text = data.Rows[2][1].ToString();
+            txHoa.Text = data.Rows[3][1].ToString();
+            txSinh.Text = data.Rows[4][1].ToString();
+            txVan.Text = data.Rows[5][1].ToString();
+
+        }
+        
     }
 }
