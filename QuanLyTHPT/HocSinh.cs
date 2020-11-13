30108e093bfe2fa8321a36224908767b67ffb3cf
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-13 15:07:40 +0700

    BT_ button lam moi

--- QuanLyTHPT/HocSinh.cs
+++ QuanLyTHPT/HocSinh.cs
@@ -43,6 +43,34 @@ namespace QuanLyTHPT
             dtgDSHocsinh.DataSource = dataProvider.GetDataTable(querydata);
         }
 
+        private void dtgDSHocsinh_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = dtgDSHocsinh.CurrentRow.Index;
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnThem.Enabled = false;
+            btnLamMoi.Enabled = true;
+            txMaHS.Text = dtgDSHocsinh.Rows[i].Cells[0].Value.ToString();
+            txTenHS.Text = dtgDSHocsinh.Rows[i].Cells[1].Value.ToString();
+            if (dtgDSHocsinh.Rows[i].Cells[2].Value.ToString() == "Nam")
+            {
+                rdbNam.Checked = true;
+                rdbNu.Checked = false;
+                rdbNam.Enabled = true;
+                rdbNu.Enabled = true;
+            }
+            if (dtgDSHocsinh.Rows[i].Cells[2].Value.ToString() == "Nữ")
+            {
+                rdbNu.Checked = true;
+                rdbNam.Checked = false;
+                rdbNam.Enabled = true;
+                rdbNu.Enabled = true;
+            }
+            dtpHS.Text = dtgDSHocsinh.Rows[i].Cells[3].Value.ToString();
+            txDiaChi.Text = dtgDSHocsinh.Rows[i].Cells[4].Value.ToString();
+            txMaLop.Text = dtgDSHocsinh.Rows[i].Cells[5].Value.ToString();
+        }
+
         private void btnSua_Click_1(object sender, EventArgs e)
         {
 
